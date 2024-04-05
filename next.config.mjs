ee98dcd912243ed8fb24f5fc7603e59e95347eb5
Joshi Divya <divyajoshics09@gmail.com>
2024-04-05 13:41:28 +0300

    removed next export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output:'export',
   basePath: '/ChitChatBot',
   assetPrefix: '/ChitChatBot',
   // Other configurations
